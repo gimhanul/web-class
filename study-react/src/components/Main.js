d9a060d1db4a6515112bc7e04c9ef9f53fa7b560
gimhanul <gimhanuly@gmail.com>
2022-04-12 16:53:24 +0900

    STUDY :: Fun facts about React

---
+++ study-react/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Was well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thounsands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
--- study-react/src/index.js
+++ study-react/src/index.js
@@ -1,18 +1,17 @@
-import React from "react"
-import ReactDom from "react-dom"
-import "./index.css"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import MainContents from "./components/MainContents"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function Page() {
-  return (
-    <div>
-      <Header /> 
-        <MainContents />
-      <Footer />
-    </div>
-  )
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-ReactDom.render(<Page />, document.getElementById("root"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
