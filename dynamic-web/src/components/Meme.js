50450316bc52ad37aeee067bdd9cdea38c4a7890
gimhanul <gimhanuly@gmail.com>
2022-05-24 11:42:12 +0900

    ADD :: meme 완성

--- dynamic-web/src/components/Meme.js
+++ dynamic-web/src/components/Meme.js
@@ -20,14 +20,42 @@ export default function Meme () {
         }));
     }
 
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setMemeImage(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            };
+        });
+    }
+
     return (
         <main>
             <div className="form">
-                <input type="text" className="form--input" placeholder="Top"/>
-                <input type="text" className="form--input" placeholder="Bottom"/>
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Top"
+                    value={meme.topText}
+                    onChange={handleChange}
+                    name="topText"
+                />
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Bottom"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                    name="bottomText"
+                />
                 <button onClick={ getMemeImage } className="form--button">Get a new meme image  🖼</button>
             </div>
-            <img className="" src={ meme.randomImage } alt="meme" />
+            <div className="meme">
+                <img className="meme--image" src={ meme.randomImage } alt="meme" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
